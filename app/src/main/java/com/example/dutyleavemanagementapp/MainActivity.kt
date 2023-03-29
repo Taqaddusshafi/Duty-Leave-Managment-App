eb185369c5d27a2e8cfb75207dfa1fbbf9db6b1e
Taqaddus <taqaddusshafi9086@gmail.com>
2023-03-30 04:44:10 +0530

    splash_screen

--- app/src/main/java/com/example/dutyleavemanagementapp/MainActivity.kt
+++ app/src/main/java/com/example/dutyleavemanagementapp/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.dutyleavemanagementapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.ImageButton
 
@@ -10,12 +13,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var btn =findViewById<ImageButton>(R.id.getst)
 
-        btn.setOnClickListener() {
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler(Looper.getMainLooper()).postDelayed({
             var intent = Intent(applicationContext, Login1::class.java)
             startActivity(intent)
-        }
+            finish()
+        }, 5000)
+
 
     }
 }
